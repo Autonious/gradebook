32d21da7bf662deb0c9879fb6cd4ab98b5510db7
Datwon Antonio Mitchell <81874483+Autonious@users.noreply.github.com>
2021-05-14 18:59:04 -0500

    Moved EnterGrades out of the main method

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,26 @@ namespace GradeBook
     {
         static void Main(string[] args) // Method named "Main"
         {
-            var book = new Book("Datwon's Grade Book");
+            IBook book = new DiskBook("Datwon's Grade Book");
             book.GradeAdded += OnGradeAdded;
+            EnterGrades(book);
 
+            var stats = book.GetStats();
 
-            while(true)
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The highest grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
             {
                 Console.WriteLine("Enter a grade or 'q' to quit.");
                 var input = Console.ReadLine(); // allow the user to enter grades
-                if(input == "q")
+                if (input == "q")
                 {
                     break;
                 }
@@ -26,11 +37,11 @@ namespace GradeBook
                     var grade = double.Parse(input); // parse input as double
                     book.AddGrade(grade);
                 }
-                catch(ArgumentException ex)
+                catch (ArgumentException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-                catch(FormatException ex)
+                catch (FormatException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
@@ -39,15 +50,6 @@ namespace GradeBook
                     Console.WriteLine("**");
                 }
             }
-
-            var stats = book.GetStats();
-
-            Console.WriteLine(Book.CATEGORY);
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
-            Console.WriteLine($"The highest grade is {stats.Average:N1}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
