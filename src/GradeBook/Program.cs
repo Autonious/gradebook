39ec58774130b6787f37b9a5817c31a0841eaf66
autonious <autonious@outlook.com>
2021-05-14 09:51:48 -0500

    added user ability to input a grade value in the console, worked with try, catch, & finally, understanding specific exception handling. added letter grade to console output for grade avg.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,15 +9,42 @@ namespace GradeBook
         static void Main(string[] args) // Method named "Main"
         {
             var book = new Book("Datwon's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.1);
-            book.AddGrade(77.5);
+
+                     
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit.");
+                var input = Console.ReadLine(); // allow the user to enter grades
+                if(input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input); // parse input as double
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
 
             var stats = book.GetStats();
 
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The highest grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
